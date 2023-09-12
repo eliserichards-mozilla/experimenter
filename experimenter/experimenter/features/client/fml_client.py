0af9250db841fcf50d731bf808c355b4efe015e7
Elise Richards <erichards@mozilla.com>
2023-09-11 17:57:16 -0700

    For #9388 feat(project): Parse yaml config files with application name and find fml repo and path

--- experimenter/experimenter/features/client/fml_client.py
+++ experimenter/experimenter/features/client/fml_client.py
@@ -1,19 +1,67 @@
 import logging
+import os
+import yaml
+
+from nimbus-experimenter import FmlClient
 
 logger = logging.getLogger()
 
-class FmlClient():
-    def create(): 
-        # todo: create FML client, https://mozilla-hub.atlassian.net/browse/EXP-3791
-        logger.info("FML client created")
-    
-    def get_feature_ids(self, versions):
-        ids = []
-        for v in versions:
-            ids.append(self.get_feature_ids())
-        return ids
+class FeatureDescriptor:
+  def id(): str
+  def description(): str
+#   def hash(): usize 
 
-    def fetch_features_for_versions(self, features, versions):
+class NimbusFmlClient():    
+    def __init__(self, application: str, channel: str, versions: list[str]): 
+        self.application: str = application
+        self.channel: str = channel
+        self.versions: list[str] = versions
+        
+        fml_local = ""
+        path :str = os.path.join("../configs", f"{application}.yaml")
+
+        if os.path.exists(path):
+            with open(path) as application_yaml_file:
+                application_data = yaml.load(
+                    application_yaml_file.read(), Loader=yaml.Loader
+                )
+                for feature_slug in application_data:
+                    if(application_data[feature_slug]) is application:
+                        fml_local = application_data["repo"] + application_data["fml_path"]
+        if fml_local != "":
+            self.fml_client = FmlClient.new(path, channel)
+            logger.info("FML client created")
+            # features: list[str] = self.fml_client.get_feature_ids()
+            # for id in features:
+            #     descriptor: FeatureDescriptor = self.fml_client.get_feature_descriptor(id)
+            
+            # features: list[FeatureDescriptor] = fm.get_features()
+        else: 
+            logger.error("Failed to find fml path: " + path)
+    
+    def get_features(self, versions):
+        if self.fml_client == "":
+            logger.error("No FML client, can\'t fetch manifests")
+            return {}
+        else:
+            ids = {}
+            for v in versions:
+                manifests = self.get_manifests(v)
+                for m in manifests:
+                    ids.update({v: m.get_feature_ids()})
+            return ids
+    
+    def get_feature_description(self, feature_id):
+        return self.get_feature_descriptor(feature_id)
+
+    def get_manifests(self, versions):
+        manifests = []
+        for v in versions:
+            manifests.append(self.get_manifest(v))
+        return manifests
+
+    def fetch_features_for_versions(self, versions):
+        features = self.get_feature_ids(versions)
         list_of_features_with_any_versions = {}
         for f in features:
             versions_set = set(features[f]).intersection(set(versions))
@@ -21,14 +69,11 @@ class FmlClient():
                 list_of_features_with_any_versions[f] = sorted(list(versions_set))
         return list_of_features_with_any_versions
 
-# {
-#   feature1: [112, 113]
-#   feature2: [112]
-#   feature3: [111]
-#   feature4: [111, 112, 113]
-# }
-
-# {
-#   111: [feature3, feature4]
-#   112: [feature1, feature3, feature4]
-# }
+    def fetch_features_for_versions(self, versions):
+        features = self.get_feature_ids(versions)
+        list_of_features_with_any_versions = {}
+        for f in features:
+            versions_set = set(features[f]).intersection(set(versions))
+            if versions_set:
+                list_of_features_with_any_versions[f] = sorted(list(versions_set))
+        return list_of_features_with_any_versions
--- experimenter/experimenter/features/tests/client/test_client.py
+++ experimenter/experimenter/features/tests/client/test_client.py
@@ -2,7 +2,8 @@ import json
 
 from django.core.checks import Error
 from django.test import TestCase
-from experimenter.features.client.fml_client import FmlClient
+from experimenter.features.client.fml_client import NimbusFmlClient
+from unittest.mock import MagicMock, patch
 
 from experimenter.features import (
     Features,
@@ -20,8 +21,34 @@ class TestClient(TestCase):
         super().setUpClass()
         Features.clear_cache()
 
+    @patch("nimbus-experimenter.fml")
+    def test_intiate_new_fml_client_no_error(self):
+        application = "fenix"
+        channel = "production"
+
+        mock_fml = MagicMock()
+        mock_fml_client.application = 
+        mock_fml_client.json.return_value = {"data": []}
+        client = NimbusFmlClient(application, channel)
+
+        self.assertEqual(client.application, application)
+        self.assertEqual(client.channel, channel)
+        self.assertEqual(client.fml_client, "")
+        
+    def test_intiate_invalid_fml_client_errors(self):
+        application = "rats"
+        channel = "production"
+        with patch("FmlClient.new") as mock_fml_new:
+            try {
+                client = NimbusFmlClient(application, channel)
+            } catch (e: Error) {
+                self.assertEqual(e, "Failed to find fml path: ../configs/rats.yaml")
+            }
+        self.assertEqual(len(mock_fml_new.mock_calls), 1)
+
+
     def test_filter_features(self):
-        client = FmlClient()
+        client = NimbusFmlClient(application="fenix", channel="production")
         
         features = {
             "feature1": [112, 113],
@@ -37,4 +64,3 @@ class TestClient(TestCase):
             list_of_features_with_versions,
             {"feature1": [112, 113], "feature2": [112], "feature4": [112, 113], }
         )
-
